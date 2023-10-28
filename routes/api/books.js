55e66ed5b7e621b25adda8c43dd61e2b3cbc61fe
sainath-kadam <mydearsainathkadam@gmail.com>
2023-10-28 23:28:22 +0530

    done

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-
 const Book = require('../../models/Book');
 router.get('/test', (req, res) => res.send('book route testing!'));
 
@@ -16,14 +15,11 @@ router.get('/:id', (req, res) => {
     .catch(err => res.status(404).json({ nobookfound: 'No Book found' }));
 });
 
-
-
 router.post('/', (req, res) => {
   Book.create(req.body)
     .then(book => res.json({ msg: 'Book added successfully' }))
     .catch(err => res.status(400).json({ error: 'Unable to add this book' }));
 });
-
 
 router.put('/:id', (req, res) => {
   Book.findByIdAndUpdate(req.params.id, req.body)
