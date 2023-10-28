c3ad7cd52ae5be1f97e580dfe60337241dfb0e84
sainath-kadam <mydearsainathkadam@gmail.com>
2023-10-28 23:24:50 +0530

    API Generated

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const connectDB = require('./config/db');
+var cors = require('cors');
+const router = express.Router();
+const books = require('./routes/api/books');
+ 
+const app = express();
 
+// Connect Database
+connectDB();
+ 
+// cors
+app.use(cors({ origin: true, credentials: true }));
+
+app.use('/api/books', books);
 
 const port = process.env.PORT || 8082;
-
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
